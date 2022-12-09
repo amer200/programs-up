028d6b1559dddd53e966f85456d98fb0a33ca8ec
amer200 <amermostaafa@gmail.com>
2022-12-09 22:06:10 +0200

    commit

--- controllers/main.js
+++ controllers/main.js
@@ -3,7 +3,28 @@ const Aritcal = require('../models/article');
 
 exports.getMainPage = async (req, res) => {
     const categs = await Categ.find();
+    const articels = await Aritcal.find();
+    let slides = [articels[0], articels[1], articels[2]];
     res.render('main/index', {
-        categs: categs
+        categs: categs,
+        articels: articels,
+        slides: slides
     });
+}
+exports.getArtical = async (req, res) => {
+    const id = req.params.id;
+    const categs = await Categ.find();
+    const aritcal = await Aritcal.findById(id);
+    res.render('main/articale', {
+        categs: categs,
+        a: aritcal
+    })
+}
+exports.Search = async (req, res) => {
+    let payload = req.body.payload.trim();
+    let search = await Aritcal.find({ name: { $regex: new RegExp() payload, $options: 'i' } });
+    // search = search.slice(0, 10);
+    res.send({
+        payload: search
+    })
 }
